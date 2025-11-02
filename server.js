96cf54ca1f8570dea13c4b1258b4efefa1b57d0e
Chandra Prakash <chandraprakash@chandraprakashs-MacBook-Air.local>
2025-11-03 00:08:10 +0530

    resolve cors problem

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173", // Development frontend
-      "https://contact-management-system-frontend.vercel.app/" // Production frontend (replace with actual)
+      "https://contact-management-system-frontend.vercel.app" // Production frontend (replace with actual)
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
